19e4cbf26de7baf4d845d59d6e3dff23fe4bb45b
Kamal Rhrabla <kamalghrabla@gmail.com>
2023-01-02 11:33:53 +0100

    add advantages

---
+++ client/src/components/advantage.jsx
@@ -0,0 +1,122 @@
+const Advantage = ()=>{
+    return(
+<div class="container my-12 px-6 mx-auto">
+<h2 class="text-3xl font-bold mb-12 text-center">Our advantages</h2>
+  <section class="mb-12 text-gray-800 text-center lg:text-left">
+    <div class="md:px-6 py-8 md:px-12">
+      <div class="container mx-auto xl:px-32">
+        <div class="grid lg:grid-cols-2 flex items-center">
+          <div class="md:mt-8 lg:mt-0 mb-8 lg:mb-0">
+            <div class="relative block rounded-lg shadow-lg px-6 pt-8 pb-4 md:px-12 lg:-mr-14" >
+              <h2 class="text-3xl font-bold mb-6">Why is it so great?</h2>
+              <p class="text-gray-500 mb-6">
+                Nunc tincidunt vulputate elit. Mauris varius purus malesuada neque iaculis
+                malesuada. Aenean gravida magna orci, non efficitur est porta id. Donec magna
+                diam.
+              </p>
+
+              <div class="grid md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-x-6">
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Support 24/7
+                  </p>
+                </div>
+
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Analytics
+                  </p>
+                </div>
+
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Components
+                  </p>
+                </div>
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Updates
+                  </p>
+                </div>
+
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Reports
+                  </p>
+                </div>
+
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Mobile
+                  </p>
+                </div>
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Modules
+                  </p>
+                </div>
+
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Blocks
+                  </p>
+                </div>
+
+                <div class="mb-6">
+                  <p class="flex items-center">
+                    <svg class="w-4 h-4 mr-2 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z">
+                      </path>
+                    </svg>Templates
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="md:mb-12 lg:mb-0">
+            <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/121.jpg"
+              class="w-full rounded-lg shadow-lg rotate-lg-6" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
+    )
+}
+
+export default Advantage;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,14 @@
+import Advantage from "../components/advantage";
 import Contact from "../components/contact";
 import Header from "../components/header";
-import Places from "../components/places"
+import Places from "../components/places";
 
 const Home = ()=>{
     return(
         <div>
             <Header/>
             <Places/>
+            <Advantage/>
             <Contact/>
         </div>
     )
