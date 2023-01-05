efc3a1ead5fa0f279425eb3686d4c0a0bbf85760
Kamal Rhrabla <kamalghrabla@gmail.com>
2023-01-05 11:30:22 +0100

    client data

---
+++ controllers/data.js
@@ -0,0 +1,62 @@
+const dataService = require("../services/data");
+const AppError = require("../helpers/appError");
+module.exports = class data {
+  static async apiGetAlldata(req, res, next) {
+    try {
+      const data = await dataService.getAlldata(); 
+      if (!datas) {
+        res.status(404).json({message: "There are no data found yet!"});
+      }else{
+        res.json(data);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiGetdataById(req, res, next) { 
+    try {
+      let id = req.params.id || {};
+      const data = await dataService.getdatabyId(id);
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiCreatedata(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+      const createddata =  await dataService.createdata(req.body);
+      res.json({
+          data: createddata
+      });
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiUpdatedata(req, res, next) {
+    try {
+      if (!req.body) return next(new AppError("No form data found", 404));
+      const updateddata = await dataService.updatedata(req.params.id,req.body);
+      if (updateddata.modifiedCount === 0) {
+        throw new Error("Unable to update data, error occord");
+      }
+      res.json(updateddata);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiDeletedata(req, res, next) {
+    try {
+      const dataId = req.params.id;
+      const deleteResponse = await dataService.deletedata(dataId);
+      res.json(deleteResponse);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+ 
+};
---
+++ services/data.js
@@ -0,0 +1,71 @@
+const data = require("../models/data"); 
+
+module.exports = class dataService{
+    static async getAlldata(){
+        try {
+            const alldatas = await data.find(); 
+            return alldatas;
+        } catch (error) {
+            console.log(`Could not fetch datas ${error}`)
+        }
+    } 
+
+    static async createdata(data){
+        try {
+            const newdata = {
+                cin: data.cin,  
+                phone: data.phone,
+                country: data.country,
+                city: data.city,
+                adresse: data.adresse,
+                born_date: data.born_date,
+                image: data.image,
+                id_client: data.id_client
+            }
+           const response = await new data(newdata).save();
+           return response;
+        } catch (error) {
+            console.log(error);
+        } 
+    }
+
+    static async getdatabyId(dataId){
+        try {
+            const singledataResponse =  await data.findById({_id: dataId});
+            return singledataResponse;
+        } catch (error) {
+            console.log(`data not found. ${error}`)
+        }
+    }
+
+    static async updatedata(data){
+            try {
+                const newdata = {
+                    cin: data.cin,  
+                    phone: data.phone,
+                    country: data.country,
+                    city: data.city,
+                    adresse: data.adresse,
+                    born_date: data.born_date,
+                    image: data.image,
+                    id_client: data.id_client
+                }
+                const updateResponse =  await data.findByIdAndUpdate({_id: data._id},newdata);
+                    return updateResponse;
+            } catch (error) {
+                console.log(`Could not update data ${error}` );
+
+        }
+    }
+
+    static async deletedata(dataId){
+        try {
+            const deletedResponse = await data.findByIdAndDelete(dataId);
+            return deletedResponse;
+        } catch (error) {
+            console.log(`Could  ot delete data ${error}`);
+        }
+
+    }
+    
+}
