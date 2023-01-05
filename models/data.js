9c691c8fedcdd8e711b635ad9d2602bfc98439c4
Kamal Rhrabla <kamalghrabla@gmail.com>
2023-01-05 10:52:38 +0100

    client data table

---
+++ models/data.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dataSchema = Schema(
+  {
+    cin: { 
+      type: String,
+      required: true,
+    },
+    phone: { 
+      type: String,
+      required: true,
+    },
+    country: { 
+      type: String,
+      required: true,
+    },
+    city: { 
+      type: String,
+      required: true,
+    },
+    adresse: { 
+      type: String,
+      required: true,
+    },
+    born_date: {
+      type: String,
+      required: true,
+    },
+    image: { 
+      type: String,
+      required: true,
+    },
+    id_client:  [{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref:'client'
+      }],
+  },
+  { timestamps: true }
+);
+
+module.exports = data = mongoose.model("data", dataSchema);
